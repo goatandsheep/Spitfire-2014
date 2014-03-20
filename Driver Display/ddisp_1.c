ef621c6f94698749e38f21c5961e4021a8b1693f
PacificCod <lil35@mcmaster.ca>
2014-03-20 17:25:01 -0400

    Added BPS function and BPS receive in main

--- Driver Display/ddisp_1.c
+++ Driver Display/ddisp_1.c
@@ -14,7 +14,9 @@
 #FUSES NOPROTECT
 
 #use delay(clock=20000000)
-#USE SPI (MASTER, CLK=PIN_C3, DI=PIN_C5, DO=PIN_C4, MODE=0, BITS=8, STREAM=SPI_1, MSB_FIRST)
+#use SPI(MASTER, CLK=PIN_C3, DI=PIN_C5, DO=PIN_C4, MODE=0, BITS=8, STREAM=SPI_1, MSB_FIRST)
+#define LED PIN_C0
+#define RTS PIN_C5
 
 //Defines
 #define LE PIN_C0
@@ -22,6 +24,7 @@
 #define BUZZER PIN_C2
 //#define LED PIN_A5
 
+#include <can-18F4580_mscp.c>  // Modified CAN library includes default FIFO mode, timing settings match MPPT, 11-bit instead of 24-bit addressing
 // Typedefs
 typedef unsigned int8 uint8;
 typedef unsigned int16 uint16;
@@ -56,7 +59,7 @@ int segment[] = {0xEE,0x82,0xDC,0xD6,0xB2,0x76,0x7E,0xC2,0xFE,0xF6, 0x2C, 0xBA};
 int segChar[] = {0x2C,0xEE,0xBA,0x82}; // L, O, H, I
 
 int8 dial;
-
+int1 BPSWarn[4];
 
 void update(signed int8 data);//write speed prototype
 void flash(signed int8 data); //flash LEDs prototype
@@ -71,32 +74,36 @@ void main() {
     setup_comparator(NC_NC_NC_NC);// This device COMP currently not supported by the PICWizard
     setup_spi(SPI_MASTER|SPI_SCK_IDLE_LOW|SPI_XMIT_L_TO_H|SPI_CLK_DIV_16|SPI_SAMPLE_AT_MIDDLE);
     
+    can_init();
+    set_tris_b((*0xF93 & 0xFB) | 0x08);  //b3 is out, b2 is in (default)
+    //set_tris_c((*0xF94 & 0xBF) | 0x80);  //c6 is out, c7 is in (if using #FUSES CANC)
     
     while(1){
-        //set_adc_channel(10);
-        //delay_us(10);
+        output_high(RTS);
         dial = read_adc(); //pot2 is 16bit [0,65535]
-        //dial=dial/65535*100;
         
         output_low(OE);
         output_low(LE);
         
         flash(dial);
-        
-        //output_high(LED);
-        //delay_ms(500);
-        //output_low(LED);
-        //delay_ms(500);
-        // testing the buzzer
-        /*output_high(Buzz);
-         delay_ms(500);
-         output_low(buzz);
-         delay_ms(500);
-         */
+        if (can_kbhit()) {
+            // If data is waiting in buffer...
+            if(can_getd(rx_id, in_data, rx_len, rxstat)) {
+
+                // get data from buffer, and place it into the fields: rx_id, in_data, rx_len... etc
+                printf("RECIEVED: BUFF=%U ID=%3LX LEN=%U OVF=%U ", rxstat.buffer, rx_id, rx_len, rxstat.err_ovfl);
+                printf("FILT=%U RTR=%U EXT=%U INV=%U\r\n", rxstat.filthit, rxstat.rtr, rxstat.ext, rxstat.inv);
+                printf("\tDATA = ");
+                for (i=0;i<rx_len;i++)
+                    printf("%02X ", in_data[i]);
+            } else {
+                printf("FAIL on can_getd");
+            }           
+            printf("\r\n");
+        }
     }
 }
 
-int1 BPSWarn[4];
 void setBPSWarn() {
     BPSWarn = {0,0,0,0};
     if(in_data[0] | in_data[1]&0xFC)
